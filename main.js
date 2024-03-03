bb855c04db284c08d222a043164db91fa4e96043
unknown <cladiifsaalax@gmail.com>
2024-03-03 11:38:09 -0800

    add all tutorial day1

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 console.log("mohmed")
+
+variable
+
+var x  = "Mohmed"
+console.log(x)
+
+x =  "yusuf"
+var  x  = "Ali"
+console.log(x)
+
+let 
+
+let xisaab = 12 + 2;
+console.log(xisaab)
+
+xisaab = 12*2
+console.log(xisaab)
+
+const
+
+const  PI  =  3.14;
+console.log(PI)
+
+PI = 24.5
+
+maxaa faribale noqo kara(_, $ , cr1, x)
+
+let  _  = "macag"
+let $   = "dollar"
+let  cr7  = " Rolando"
+let x = "abdilaidf"
+
+maxaa noqn karin (my-name, let, 4m)
